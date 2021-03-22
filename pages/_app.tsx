6ebb91e61d258662320ed879640eead18be16b3d
Jackson <tongtoan2704@gmail.com>
2021-03-22 11:57:34 +0700

    integrate firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,19 @@
+import firebase from "firebase/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2AaEGYFZ0va6ZI8RKKX7ryXwLsaj21jM",
+  authDomain: "fir-nextjs-4de43.firebaseapp.com",
+  databaseURL: "https://fir-nextjs-4de43.firebaseio.com",
+  projectId: "fir-nextjs-4de43",
+  storageBucket: "fir-nextjs-4de43.appspot.com",
+  messagingSenderId: "507862495192",
+  appId: "1:507862495192:web:2054982e463765437dccad",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const MyApp = ({ Component, pageProps }) => {
   return (
